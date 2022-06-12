fb0ee7a9712c84949efb8708302ace9344d44dd1
jinho1011 <gino9940@gmail.com>
2022-06-12 21:15:06 +0900

    Initial Commit

---
+++ Sullog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sullog
+//
+//  Created by 전진호 on 2022/06/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
